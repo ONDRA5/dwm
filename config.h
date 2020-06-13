d81121d91e5b8edcadeca849f4c6dbcc8a7c767c
ONDRA5 <ondra.halama@gmail.com>
2020-06-14 00:36:48 +0200

    add the ability to move windows

--- config.h
+++ config.h
@@ -80,6 +80,7 @@ static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34
 //static const char *volup[] = { "amixer", "set", "Master", "10%+", NULL };
 
 #include <X11/XF86keysym.h>
+#include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
@@ -105,6 +106,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY|ShiftMask,		XK_j,	   movestack,	   {.i = +1 } },
+	{ MODKEY|ShiftMask,		XK_k,	   movestack,	   {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
