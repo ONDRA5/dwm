dfe79c1950bfb6400970e82c774b623cecb9d856
ONDRA5 <ondra.halama@gmail.com>
2020-06-15 22:06:51 +0200

    minor changes?

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;	/* 0 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]  = { "Hack Nerd Font:size=10:antialias=true:autohint=true", "fontawesome:size=10" };
+static const char *fonts[]  = { "Hack Nerd Font:size=10:antialias=true:autohint=true", "FontAwesome:size=10" };
 static const char dmenufont[]       = "Hack Nerd Font:size=10";
 static const char col_gray1[]       = "#282828";	/* bar background color */
 static const char col_gray2[]       = "#444444";	/* unfocused window frame color */
